1c239e4f0787201781a0813136f5977bb5dd0c37
William Thio <williamthio886@gmail.com>
2025-02-28 00:04:57 +0700

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -1,8 +1,8 @@
 import csv
 from collections import defaultdict, Counter
 
-MAIN_POKEMON = "noctowl"
-SECONDARY_POKEMON = "charizard"
+MAIN_POKEMON = "charizard"
+SECONDARY_POKEMON = None
 MIN_RANK = 4
 
 def parse_deck_section(cards):
